4e795ef7b04a8bb2a97f04bbe34e7470bd718f92
ViniciusBazan <vinicius.bazan16@gmail.com>
2021-05-09 16:56:22 -0300

    error BarChart

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,10 @@
 export type SaleSum = {
-    sellerName: string,
-    num: number
+    sellerName: string;
+    num: number;
+}
+
+export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
 }
